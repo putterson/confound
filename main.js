a6633311e2de1b2ec620625f2f367db54b8a4fb3
Pat Goebel <putterson@gmail.com>
2016-10-01 17:25:23 -0400

    Layout and randomized letters

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function dieRoll() {
+
+  return String.fromCharCode('A'.charCodeAt(0) +  Math.floor((Math.random() * 26)));
+}
+
+function rollDice() {
+  $( ".die-content" ).each( function(idx) {
+    $( this ).text(dieRoll());
+    console.log( this);
+  });
+}
+
+$( document ).ready( function() {
+  $( "#roll" ).onclick=rollDice;
+  rollDice();  
+});
